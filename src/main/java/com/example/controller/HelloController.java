255933b0985e0ceb0c6bf5e5cd6611523609ad96
Lev jack <lev_jack@mail.ru>
2021-08-25 10:49:41 +0300

    test commit

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HelloController {
-
+	//some changes
 //    private UserServiceImpl userService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
