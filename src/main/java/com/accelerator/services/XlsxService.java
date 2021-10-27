4248d1a0c45e38265b574879236aaf7a43aa7373
Tihon Kasko <t.kasko@itransition.com>
2021-10-27 10:44:05 +0300

    AC-21: Updated filling alhoritm, added mother's 2D .xlsx file.

--- src/main/java/com/accelerator/services/XlsxService.java
+++ src/main/java/com/accelerator/services/XlsxService.java
@@ -5,6 +5,7 @@ import org.apache.poi.xssf.model.SharedStringsTable;
 import javax.xml.stream.XMLEventFactory;
 import javax.xml.stream.XMLEventWriter;
 import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
 import java.util.List;
 
 public interface XlsxService {
@@ -13,4 +14,13 @@ public interface XlsxService {
                                         SharedStringsTable sharedstringstable,
                                         XMLEventWriter writer,
                                         List<String> values) throws XMLStreamException;
+
+    XMLEventWriter writeValueInNewCell(XMLEventFactory eventFactory,
+                                       SharedStringsTable sharedstringstable,
+                                       XMLEventWriter writer,
+                                       String value) throws XMLStreamException;
+
+    XMLEvent prepareFillingValueEvent(XMLEventFactory eventFactory,
+                                      SharedStringsTable sharedstringstable,
+                                      String value);
 }
